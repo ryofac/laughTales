e5529928d89ce668a917ef0529d75f3256fdec7c
ryofac <ryanfaustinocarvalho@gmail.com>
2024-01-28 20:39:17 -0300

    feat - gamejam version finished

--- Scripts/Itens/joy_crystal.gd
+++ Scripts/Itens/joy_crystal.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var joy_factor: int = 50;
-
+@onready var stream = $AudioStreamPlayer2D as AudioStreamPlayer2D
 var used: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +9,9 @@ func _ready():
 
 func _on_body_entered(body):
 	if body is Player:
+		stream.play();
 		if !used:
 			body.regaining_joy(joy_factor)
+			visible = false;
+			await stream.finished
 			queue_free();
--- Scripts/Itens/life_crystal.gd
+++ Scripts/Itens/life_crystal.gd
@@ -3,12 +3,16 @@ extends Node2D
 @export var HEALING_FACTOR: int = 1;
 
 var used: bool = false;
+@onready var stream = $AudioStreamPlayer2D as AudioStreamPlayer2D
 
 func _ready():
 	pass
 
 func _on_body_entered(body):
 	if body is Player:
+		stream.play();
 		if !used:
 			body.healing(HEALING_FACTOR);
+			visible = false;
+			await stream.finished
 			queue_free();
